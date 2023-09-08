f27805c7f138ea05b71057206f515ef87481991f
AKHILESH GUNDA <akhileshgunda11@gmail.com>
2023-09-08 15:03:34 +0530

    first commit

--- frontens.sh
+++ frontens.sh
@@ -1,11 +1,11 @@
-dnf install nginx -y
+echo dnf install nginx -y
 
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cp friday.conf  /etc/nginx/default.d/friday.conf
+echo systemctl enable nginx
+echo systemctl start nginx
+echo rm -rf /usr/share/nginx/html/*
+echo curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo cp friday.conf  /etc/nginx/default.d/friday.conf
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl restart nginx
+echo cd /usr/share/nginx/html
+echo unzip /tmp/frontend.zip
+echo systemctl restart nginxecho
