abfa3c719e2620d56fc31376987744d3951cecd9
AKHILESH GUNDA <akhileshgunda11@gmail.com>
2023-09-08 21:52:19 +0530

    first commit

--- frontens.sh
+++ frontens.sh
@@ -7,7 +7,7 @@ echo systemctl start nginx
 echo rm -rf /usr/share/nginx/html/*
 echo curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 echo cp friday.conf  /etc/nginx/default.d/friday.conf
-
+echo $?
 echo cd /usr/share/nginx/html
 echo unzip /tmp/frontend.zip >>akhil_file
 echo systemctl restart nginxecho
